6562eeb2eb1425b23b2091a9f3fd5c4d1935b71e
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-24 10:02:52 +0530

    Runtime: 18 ms (faster than 64.47%), Memory: 31.4 MB (less than 66.17%) - GitSync

---
+++ 724-find-pivot-index/724. Find Pivot Index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int leftSum = 0, rightSum = 0; 
+
+        for (int &x : nums) rightSum += x; 
+
+        for (int i = 0; i < nums.size(); i++){
+            rightSum -= nums[i];
+            if (leftSum == rightSum) return i;
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+};
