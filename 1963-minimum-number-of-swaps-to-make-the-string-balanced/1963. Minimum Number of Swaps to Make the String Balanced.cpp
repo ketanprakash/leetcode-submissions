919ecaf9f3f39ec85d409847f78ece33bf9f17d6
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-24 10:31:32 +0530

    Runtime: 97 ms (faster than 82.84%), Memory: 31.6 MB (less than 63.01%) - GitSync

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963. Minimum Number of Swaps to Make the String Balanced.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int close = 0, maxClose = 0; 
+
+        for (int i = 0; i < s.size(); i++){
+            if (s[i] == ']'){
+                close++;
+            }
+            else {
+                close--; 
+            }
+
+            maxClose = max(close, maxClose);
+        }
+
+        return (maxClose + 1) / 2;
+    }
+};
