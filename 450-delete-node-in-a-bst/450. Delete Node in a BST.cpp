96a472e2be6e71dd477b2a57c52c19483684c6c2
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-24 09:54:32 +0530

    Runtime: 21 ms (faster than 82.07%), Memory: 33 MB (less than 67.6%) - GitSync

---
+++ 450-delete-node-in-a-bst/450. Delete Node in a BST.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == nullptr) return root; 
+
+        if (root -> val == key){
+            if (root -> left == nullptr && root -> right == nullptr){
+                return nullptr;
+            }
+            else if (root -> left == nullptr){
+                return root -> right;
+            }
+            else if (root -> right == nullptr){
+                return root -> left;
+            }
+            else {
+                TreeNode *temp = root -> left;
+                while (temp -> right != nullptr) temp = temp -> right; 
+                root -> val = temp -> val; 
+                root -> left = deleteNode(root -> left, root -> val);
+                return root;
+            }
+        }
+        else if (root -> val < key){
+            root -> right = deleteNode(root -> right, key);
+            return root;
+        }
+        else {
+            root -> left = deleteNode(root -> left, key);
+            return root;
+        }
+    }
+};
