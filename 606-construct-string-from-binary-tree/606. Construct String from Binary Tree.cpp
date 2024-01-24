4ee20ba4cb200cd1c3323ccbc04f4b7e9da9486f
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-24 09:59:03 +0530

    Runtime: 19 ms (faster than 36.45%), Memory: 55.5 MB (less than 37.16%) - GitSync

---
+++ 606-construct-string-from-binary-tree/606. Construct String from Binary Tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        if (root == nullptr) return "";
+
+        string ans = to_string(root -> val);
+        string left = tree2str(root -> left);
+        string right = tree2str(root -> right);
+
+        if (left.size() == 0 && right.size() == 0){
+            return ans;
+        }
+        else if (right.size() == 0){
+            ans.push_back('(');
+            ans += left;
+            ans.push_back(')');
+            return ans;
+        }
+        ans.push_back('(');
+        ans += left; 
+        ans.push_back(')');
+
+        ans.push_back('(');
+        ans += right; 
+        ans.push_back(')');
+
+        return ans;
+    }
+};
