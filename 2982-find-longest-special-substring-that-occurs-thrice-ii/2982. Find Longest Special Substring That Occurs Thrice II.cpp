6ab96dff26aa008dbec0494c674e35a4944c69c2
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-24 10:50:02 +0530

    Runtime: 1692 ms (faster than 10.66%), Memory: 31.5 MB (less than 98.18%) - GitSync

---
+++ 2982-find-longest-special-substring-that-occurs-thrice-ii/2982. Find Longest Special Substring That Occurs Thrice II.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool check(string &str, int size){
+        int s = 0, e = s + size - 1; 
+        
+        int freq[26] = {0};
+        int freq2[26] = {0};
+        
+        for (int i = s; i <= e; i++) {
+            freq[str[i] - 'a']++;
+        }
+        
+        int sum = accumulate(freq, freq + 26, 0);
+        
+        for (int i = 0; i < 26; i++){
+            if (sum == freq[i]) freq2[i]++;
+        }
+        
+        while (e < str.size() - 1){
+            freq[str[s++] - 'a']--;
+            freq[str[++e] - 'a']++;
+            for (int i = 0; i < 26; i++){
+                if (sum == freq[i]) freq2[i]++;
+            }
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (freq2[i] >= 3) return true;
+        }
+        
+        return false;
+    }
+    
+    int maximumLength(string str) {
+        if (str.size() <= 2) return -1;
+        
+        int s = 1, e = str.size() - 2, m;
+        int ans = -1;
+        
+        while (s <= e){
+            m = s + (e - s) / 2; 
+            
+            if (check(str, m)){
+                ans = m; 
+                s = m + 1;
+            }
+            else {
+                e = m - 1;
+            }
+        }
+        
+        return ans;
+    }
+};
