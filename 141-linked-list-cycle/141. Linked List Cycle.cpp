524129fd0f2da35e62bae4d3c84a5d12ee0675c4
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-24 09:41:57 +0530

    Runtime: 11 ms (faster than 48.37%), Memory: 8.4 MB (less than 64.79%) - GitSync

---
+++ 141-linked-list-cycle/141. Linked List Cycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr || head -> next == nullptr) return false;
+        ListNode *slow = head, *fast = head; 
+
+        do {
+            slow = slow -> next; 
+            fast = fast -> next -> next;
+        } while (fast != nullptr && fast -> next != nullptr && fast != slow);
+
+        return fast == slow;
+    }
+};
