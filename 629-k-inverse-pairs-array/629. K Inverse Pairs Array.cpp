82234b006ca1a35ad0a49eba4672f62139d1afd1
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-27 18:47:34 +0530

    Runtime: 35 ms (45.60%), Memory: 11.2 MB (43.96%) - Gitcode

--- 629-k-inverse-pairs-array/629. K Inverse Pairs Array.cpp
+++ 629-k-inverse-pairs-array/629. K Inverse Pairs Array.cpp
@@ -3,16 +3,22 @@ int dp[1001][1001];
 class Solution {
 public:
     int mod = 1e9 + 7;
+
+    // helper(n, k)     = helper(n - 1, k) + helper(n - 1, k - 1) + ... helper(n - 1, k - n + 1)
+    // helper(n, k - 1) =                    helper(n - 1, k - 1) + ... helper(n - 1, k - n + 1) + helper(n - 1, k - n)
+
+    //helper(n, k) - helper(n, k - 1) = helper(n - 1, k) - helper(n - 1, k - n)
+    //helper(n, k) = helper(n, k - 1) + helper(n - 1, k) - helper(n - 1, k - n)
     int helper(int n, int k){
         if (k < 0) return 0;
         if (k == 0) return 1;
         if (n == 0) return 0;
 
         if (dp[n][k] != -1) return dp[n][k];
-        int ans = 0;
-        for (int i = max(1, n - k); i <= n; i++){
-            ans = (ans +  helper(n - 1, k - n + i) % mod) % mod;
-        }
+        int ans = 0; 
+        ans = (ans + helper(n, k - 1) % mod) % mod;
+        ans = (ans + helper(n - 1, k) % mod) % mod; 
+        ans = (ans - helper(n - 1, k - n) % mod + mod) % mod;
 
         return dp[n][k] = ans;
     }
