ea04346ad29265b75be3fe8a824d825ce3b6a0e5
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-01-29 00:02:31 +0530

    Runtime: 24 ms (75.52%), Memory: 19.9 MB (44.26%) - Gitcode

---
+++ 115-distinct-subsequences/115. Distinct Subsequences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int helper(string &s, string &t, int n, int m, vector<vector<int>> &dp){
+        if (m == 0) return 1; 
+        if (n == 0) return 0; 
+        
+        if (dp[n][m] != -1) return dp[n][m];
+
+        int ans = 0;
+        if (s[n - 1] == t[m - 1]){
+            ans += helper(s, t, n - 1, m - 1, dp);
+        }
+
+        ans += helper(s, t, n - 1, m, dp);
+
+        return dp[n][m] = ans;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size(); 
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
+
+        return helper(s, t, n, m, dp);
+    }
+};
