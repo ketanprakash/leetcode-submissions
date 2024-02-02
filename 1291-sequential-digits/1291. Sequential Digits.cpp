ba9d29283afa48da6d4bf11472be9786a70d3774
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-02-02 06:05:06 +0530

    Runtime: 0 ms (100.00%), Memory: 7.4 MB (8.37%) - Gitcode

--- 1291-sequential-digits/1291. Sequential Digits.cpp
+++ 1291-sequential-digits/1291. Sequential Digits.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        int lowSize = log10(low) + 1;
-        int highSize = log10(high) + 1;
+        queue<int> q; 
+        for (int i = 1; i <= 8; i++) q.push(i);
 
-        int size = lowSize; 
         int num;
         vector<int> ans;
-        while (size <= highSize){
-            for (int i = 1; i <= 10 - size; i++){
-                num = 0; 
-                for (int j = i; j <= i + size - 1; j++){
-                    num = num * 10 + j;
-                }
-                if (num >= low && num <= high) ans.push_back(num);
+        while (!q.empty()){
+            num = q.front(); 
+            q.pop(); 
+            
+            if (num >= low && num <= high) {
+                ans.push_back(num);
             }
-            size++;
+
+            if (num % 10 < 9 && num * 10 + num % 10 + 1 <= high){
+                q.push(num * 10 + num % 10 + 1);
+            }
         }
+        sort(ans.begin(), ans.end());
 
         return ans;
     }
