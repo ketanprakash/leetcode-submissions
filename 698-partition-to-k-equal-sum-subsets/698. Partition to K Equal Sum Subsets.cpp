c8a26dbf36ac79512f568502bdf457fe2d7a11a2
Ketan Prakash <77245223+ketanprakash@users.noreply.github.com>
2024-02-03 11:52:49 +0530

    Runtime: 22 ms (82.45%), Memory: 103.4 MB (5.07%) - Gitcode

--- 698-partition-to-k-equal-sum-subsets/698. Partition to K Equal Sum Subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698. Partition to K Equal Sum Subsets.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool canPartitionKSubsets(vector<int> &nums, int k, int curr_sum, int target, int idx, vector<bool> &visited){
+    bool helper(vector<int> &nums, int k, int mask, int rem, int target, vector<vector<int>> &dp){
         if (k == 1) return true;
-        if (idx == nums.size()) return false;
-        if (curr_sum == target){
-            return canPartitionKSubsets(nums, k - 1, 0, target, 0, visited);
+
+        if (dp[k][mask] != -1) return dp[k][mask];
+        for (int i = 0; i < nums.size(); i++){
+            if (mask & (1 << i)){
+                if (rem == nums[i]){
+                    if (helper(nums, k - 1, mask & (~(1 << i)), target, target, dp)) return dp[k][mask] = true;
+                }
+                else if (rem > nums[i]){
+                    if (helper(nums, k, mask & (~(1 << i)), rem - nums[i], target, dp)) return dp[k][mask] = true;
+                }
+            }
         }
 
-        if (!visited[idx] && curr_sum + nums[idx] <= target){
-            visited[idx] = true;
-            if (canPartitionKSubsets(nums, k, curr_sum + nums[idx], target, idx + 1, visited)) return true;
-            visited[idx] = false;
-        }
-
-        if (canPartitionKSubsets(nums, k, curr_sum, target, idx + 1, visited)) return true;
-
-        return false;
+        return dp[k][mask] = false;
     }
 
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        vector<bool> visited(nums.size(), false);
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (nums.size() < k || sum % k != 0) return false;
+        if (sum % k != 0) return false;
 
-        return canPartitionKSubsets(nums, k, 0, sum / k, 0, visited);
+        vector<vector<int>> dp(k + 1, vector<int>(1 << (int)(nums.size()), -1));
+        return helper(nums, k, (1 << (int)nums.size()) - 1, sum / k, sum / k, dp);
     }
 };
